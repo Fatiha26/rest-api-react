adf6f7f094e45472a0447bbf7c4cc080f423effd
Lamia Fatiha <f6lamia@gmail.com>
2024-07-14 22:30:09 +0600

    fetching the comments and posts

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { getCommentsByPostId } from '../services/api';
 
 const Comments = ({ postId }) => {
--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getPostById } from '../services/api';
 import Comments from './Comments';
